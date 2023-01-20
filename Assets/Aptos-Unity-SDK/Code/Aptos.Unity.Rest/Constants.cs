e3013fdafff6b89778be6382df6841c28d254179
kPatch <irvsteve@gmail.com>
2023-01-19 17:31:42 -0800

    Update constants

--- Assets/Aptos-Unity-SDK/Code/Aptos.Unity.Rest/Constants.cs
+++ Assets/Aptos-Unity-SDK/Code/Aptos.Unity.Rest/Constants.cs
@@ -17,6 +17,10 @@ namespace Aptos.Rest
         public const string ENTRY_FUNCTION_PAYLOAD = "entry_function_payload";
         public const string CREATE_TOKEN_SCRIPT_FUNCTION = "0x3::token::create_token_script";
         public const string CREATE_COLLECTION_SCRIPT = "0x3::token::create_collection_script";
+        public const string DIRECT_TRANSFER_SCRIPT = "0x3::token::direct_transfer_script";
+
+        public const string TOKEN_TRANSFER_OFFER_SCRIPT = "0x3::token_transfers::offer_script";
+        public const string TOKEN_TRANSFER_CLAIM_SCRIPT = "0x3::token_transfers::claim_script";
         public const string COIN_TRANSFER_FUNCTION = "0x1::coin::transfer";
         public const string APTOS_ASSET_TYPE = "0x1::aptos_coin::AptosCoin";
     }
