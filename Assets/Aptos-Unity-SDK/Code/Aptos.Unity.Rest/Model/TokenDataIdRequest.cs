a98d270575c266f6f86214296ef18cd6b894ddfe
kPatch <irvsteve@gmail.com>
2023-01-19 17:33:08 -0800

    Add data model for TokenDataID

---
+++ Assets/Aptos-Unity-SDK/Code/Aptos.Unity.Rest/Model/TokenDataIdRequest.cs
@@ -0,0 +1,16 @@
+using Newtonsoft.Json;
+
+namespace Aptos.Rest.Models
+{
+    public class TokenDataIdRequest
+    {
+        [JsonProperty("creator", Required = Required.Always)]
+        public string Creator { get; set; }
+
+        [JsonProperty("collection", Required = Required.Always)]
+        public string Collection { get; set; }
+
+        [JsonProperty("name", Required = Required.Always)]
+        public string Name { get; set; }
+    }
+}
