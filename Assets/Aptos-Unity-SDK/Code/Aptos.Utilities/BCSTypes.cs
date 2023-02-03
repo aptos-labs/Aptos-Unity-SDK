d672d38456142be65bd3c848fa3e85cedd406661
kPatch <irvsteve@gmail.com>
2023-02-03 05:57:09 -0500

    Add TODO reminder

--- Assets/Aptos-Unity-SDK/Code/Aptos.Utilities/BCSTypes.cs
+++ Assets/Aptos-Unity-SDK/Code/Aptos.Utilities/BCSTypes.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Text;
-using UnityEngine;
+using UnityEngine; // TODO: Remove before moving to main / prod
 
 namespace Aptos.Utilities.BCS
 {
