0d0fe77f452874bae62555915ac2a314f4c21ba2
kPatch <irvsteve@gmail.com>
2023-02-09 02:04:49 -0300

    Add ResponseInfo class definition

---
+++ Assets/Aptos-Unity-SDK/Code/Aptos.Unity.Rest/Model/ResponseInfo.cs
@@ -0,0 +1,15 @@
+namespace Aptos.Unity.Rest.Model
+{
+    public class ResponseInfo
+    {
+        public Status status;
+        public string message;
+        public enum Status
+        {
+            Success,
+            Pending,
+            NotFound,
+            Failed
+        }
+    }
+}
