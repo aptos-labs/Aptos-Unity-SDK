53a68ee4bc5b2c31423dda36d9804617010d0fef
kPatch <irvsteve@gmail.com>
2023-02-24 05:02:39 -0700

    Add Signature class. #40

---
+++ Assets/Aptos-Unity-SDK/Code/Aptos.Accounts/Signature.cs
@@ -0,0 +1,58 @@
+using Aptos.Utilities.BCS;
+using Chaos.NaCl;
+
+namespace Aptos.Accounts
+{
+    /// <summary>
+    /// Representation of a ED25519 signature
+    /// </summary>
+    public class Signature
+    {
+        /// <summary>
+        /// Signature length
+        /// </summary>
+        public const int SignatureLength = 64;
+
+        /// <summary>
+        /// Byte representation of the signature
+        /// </summary>
+        private readonly byte[] _signatureBytes;
+
+        /// <summary>
+        /// Hex-string representation of the signature
+        /// </summary>
+        private string _signature;
+
+        public Signature(byte[] signature)
+        {
+            _signatureBytes = signature;
+        }
+
+        /// <summary>
+        /// The signature data in 64-bytes.
+        /// </summary>
+        /// <returns>64-byte array representing the signature data</returns>
+        public byte[] Data()
+        {
+            return _signatureBytes;
+        }
+
+        /// <inheritdoc cref="ToString"/>
+        public override string ToString()
+        {
+            string signatureHex = CryptoBytes.ToHexStringLower(_signatureBytes);
+            _signature = "0x" + signatureHex;
+
+            return _signature;
+        }
+
+        /// <summary>
+        /// Serialize signature
+        /// </summary>
+        /// <param name="serializer">Serializer object</param>
+        public void Serialize(Serialization serializer)
+        {
+            serializer.SerializeBytes(this._signatureBytes);
+        }
+    }
+}
