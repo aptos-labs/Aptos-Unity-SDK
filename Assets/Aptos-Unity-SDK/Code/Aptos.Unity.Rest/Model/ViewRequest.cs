7523da4345b40c1837249e88a6b4f8a7dfdab211
Daniel Porteous <daniel@dport.me>
2023-06-13 21:31:10 +0100

    Add example of use to SimpleNftExample

--- Assets/Aptos-Unity-SDK/Code/Aptos.Unity.Rest/Model/ViewRequest.cs
+++ Assets/Aptos-Unity-SDK/Code/Aptos.Unity.Rest/Model/ViewRequest.cs
@@ -4,7 +4,6 @@ namespace Aptos.Unity.Rest.Model
 {
     /// <summary>
     /// Represents a view function request
-    /// NOTE: Arguments is serialized to a single object that contains arrays of different types
     /// </summary>
     [JsonObject]
     public class ViewRequest
