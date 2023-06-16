f1f40de52983a84fe5cb4bdd1c292379aa80982e
Irvin Steve Cardenas <irvsteve@gmail.com>
2023-06-16 14:49:10 -0400

    Update AptosToken example to use REST client instance

--- Assets/Aptos-Unity-SDK/SDK-Examples/SDK Demo/Scripts/AptosToken/AptosToken.cs
+++ Assets/Aptos-Unity-SDK/SDK-Examples/SDK Demo/Scripts/AptosToken/AptosToken.cs
@@ -19,11 +19,14 @@ namespace Aptos.Unity.Sample
         IEnumerator RunAptosClientExample()
         {
             #region REST & Faucet Client Setup
+            Debug.Log("<color=cyan>=== ========= ===</color>");
+            Debug.Log("<color=cyan>=== Set Up Faucet & REST Client ===</color>");
+            Debug.Log("<color=cyan>=== ========= ===</color>");
             string faucetEndpoint = "https://faucet.devnet.aptoslabs.com";
 
             FaucetClient faucetClient = FaucetClient.Instance;
 
-            RestClient restClient = new RestClient();
+            RestClient restClient = RestClient.Instance;
             Coroutine restClientSetupCor = StartCoroutine(RestClient.Instance.SetUp((_restClient) => {
                 restClient = _restClient;
             }, Constants.DEVNET_BASE_URL));
